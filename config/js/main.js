bff8ce40c5822192e60fcddef26366ddf4a74938
silasg <silas@graffy.info>
2016-03-19 20:22:20 +0100

    #5: slate config page

--- config/js/main.js
+++ config/js/main.js
@@ -2,6 +2,7 @@
 (function() {
  loadOptions();
  submitHandler();
+ getAppVersionAndNews();
  })();
 
 function submitHandler() {
@@ -22,12 +23,26 @@ function loadOptions() {
         var config = JSON.parse(localStorage.config);
         $('[name="light_theme"]').removeClass("active");
         $('[name="light_theme"][value="' + config.light_theme + '"]').addClass("active");
+        $('#second_hand')[0].checked = config.second_hand;
+        $('#second_night')[0].checked = config.second_night;
+        $('#second_battery')[0].checked = config.second_battery;
+        $('#date')[0].checked = config.date;
+        $('#day')[0].checked = config.day;
+        $('#bluetooth')[0].checked = config.bluetooth;
+        $('#battery')[0].checked = config.battery;
     }
 }
 
 function getConfigData() {
     var options = {
-        light_theme: $('[name="light_theme"].active')[0].attributes.value.value === 'true'
+        light_theme: $('[name="light_theme"].active')[0].attributes.value.value === 'true',
+        second_hand: $('#second_hand')[0].checked,
+        second_night: $('#second_night')[0].checked,
+        second_battery: $('#second_battery')[0].checked,
+        date: $('#date')[0].checked,
+        bluetooth: $('#bluetooth')[0].checked,
+        day: $('#day')[0].checked,
+        battery: $('#battery')[0].checked
     };
     
     
@@ -50,4 +65,34 @@ function getQueryParam(variable, defaultValue) {
         }
     }
     return defaultValue || false;
+}
+
+function ajax(url, type, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText);
+    };
+    xhr.open(type, url);
+    xhr.send();
+};
+
+function getAppVersionAndNews() {
+    ajax(
+         'https://rawgit.com/silasg/thin/master/config/app_versions.json',
+         'GET',
+         function(responseText) {
+         var json = JSON.parse(responseText);
+         $('#latest_version')[0].innerHTML = 'Latest version: ' + json.thin-ext;
+         $('#latest_news')[0].innerHTML = '' + json.thin-ext-news;
+         }
+         );
+    
+    var version = '' + document.location;
+    var index = version.indexOf('version=');
+    if(index > 0) {
+        version = version.substring(index + 'version='.length);
+        $('#current_version')[0].innerHTML = 'Current version: ' + version;
+    } else {
+        $('#current_version')[0].innerHTML = 'Error determining local version. Running in the emulator?';
+    }
 }
