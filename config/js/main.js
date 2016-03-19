050c806e961178b54cdbfbfbc2bd6c8203cacf29
silasg <silas@graffy.info>
2016-03-19 12:11:49 +0100

    #5: started slate config page

--- config/js/main.js
+++ config/js/main.js
@@ -20,13 +20,13 @@ function submitHandler() {
 function loadOptions() {
     if (localStorage.config) {
         var config = JSON.parse(localStorage.config);
-        $('#light_theme')[0].val = config.light_theme;
+        $('[name="light_theme"]')[0].val(config.light_theme);
     }
 }
 
 function getConfigData() {
     var options = {
-        light_theme: $('[name="light_theme"]')[0].val === 'true'
+        light_theme: $('[name="light_theme"]')[0].val() === 'true'
     };
     
     
